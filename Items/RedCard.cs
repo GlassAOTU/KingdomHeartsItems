049370862cecbbf8d124b255de7f6dd9ef5e3952
abaseen-khan <arbaseen@gmail.com>
2023-08-10 02:58:00 -0400

    Add project files.

---
+++ Items/RedCard.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static KingdomHeartsItems.Main;
+
+namespace KingdomHeartsItems.Items
+{
+    internal class RedCard : ItemBase
+    {
+        public override string ItemName => "Red Card";
+
+        public override string ItemLangTokenName => "RED_CARD_ITEM";
+
+        public override string ItemPickupDesc => "Serves no purpose.";
+
+        public override string ItemFullDescription => "A useless red card.";
+
+        public override string ItemLore => "Originates from Castle Oblivion.";
+
+        public override ItemTier Tier => ItemTier.Tier1;
+
+        public override GameObject ItemModel => MainAssets.LoadAsset<GameObject>("RedCardPrefab.prefab");
+
+        public override Sprite ItemIcon => MainAssets.LoadAsset<Sprite>("RedCardCommonIcon.png");
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateItem();
+            Hooks();
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,77 @@
+﻿using BepInEx;
+using KingdomHeartsItems.Items;
+using R2API;
+using R2API.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace KingdomHeartsItems
+{
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI), nameof(EliteAPI))]
+    public class Main : BaseUnityPlugin
+    {
+        public const string ModGuid = "com.GlassAOTU.KingdomHeartsItems";
+        public const string ModName = "Kingdom Hearts Items";
+        public const string ModVer = "0.1.0";
+
+        public static AssetBundle MainAssets;
+
+        public List<ItemBase> Items = new List<ItemBase>();
+
+        //Provides a direct access to this plugin's logger for use in any of your other classes.
+        public static BepInEx.Logging.ManualLogSource ModLogger;
+
+        private void Awake()
+        {
+            ModLogger = Logger;
+
+            // Don't know how to create/use an asset bundle, or don't have a unity project set up?
+            // Look here for info on how to set these up: https://github.com/KomradeSpectre/AetheriumMod/blob/rewrite-master/Tutorials/Item%20Mod%20Creation.md#unity-project
+            // (This is a bit old now, but the information on setting the unity asset bundle should be the same.)
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("KingdomHeartsItems.Assets.kingdomheartsitems_assets"))
+            {
+                MainAssets = AssetBundle.LoadFromStream(stream);
+            }
+
+            //This section automatically scans the project for all items
+            var ItemTypes = Assembly.GetExecutingAssembly().GetTypes().Where(type => !type.IsAbstract && type.IsSubclassOf(typeof(ItemBase)));
+
+            foreach (var itemType in ItemTypes)
+            {
+                ItemBase item = (ItemBase)System.Activator.CreateInstance(itemType);
+                if (ValidateItem(item, Items))
+                {
+                    item.Init(Config);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A helper to easily set up and initialize an item from your item classes if the user has it enabled in their configuration files.
+        /// <para>Additionally, it generates a configuration for each item to allow blacklisting it from AI.</para>
+        /// </summary>
+        /// <param name="item">A new instance of an ItemBase class."</param>
+        /// <param name="itemList">The list you would like to add this to if it passes the config check.</param>
+        public bool ValidateItem(ItemBase item, List<ItemBase> itemList)
+        {
+            var enabled = Config.Bind<bool>("Item: " + item.ItemName, "Enable Item?", true, "Should this item appear in runs?").Value;
+            var aiBlacklist = Config.Bind<bool>("Item: " + item.ItemName, "Blacklist Item from AI Use?", false, "Should the AI not be able to obtain this item?").Value;
+            if (enabled)
+            {
+                itemList.Add(item);
+                if (aiBlacklist)
+                {
+                    item.AIBlacklisted = true;
+                }
+            }
+            return enabled;
+        }
+    }
+}
